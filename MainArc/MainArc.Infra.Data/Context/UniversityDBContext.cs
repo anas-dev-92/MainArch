038b59214b858fdd54b807a28fd36b429b352467
Anas Mohammed <Anas_Mohammed92@outlook.com>
2019-11-20 12:30:37 +0000

    initial commit for Infra.Data ,Update Mvc for new database (UniversityDBContext)

---
+++ MainArc/MainArc.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using MainArc.Ddomain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainArc.Infra.Data.Context
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base (options)
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
